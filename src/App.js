c4e98bf18b32e4355d1a20ebe846b8ed4c2d17c2
JordonP123 <jordonaren@gmail.com>
2022-07-22 00:32:37 -0400

    done with MVP, not doing stretch, imroving NASA photo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [people, setPeople] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(()=>{
+    axios.get(`https://swapi.dev/api/people`)
+    .then(res=>{
+      setPeople(res.data.results)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }, [])
+console.log(people)
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS</h1>
+      <Character people={people} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,71 @@
 // Write your Character component here
+import react from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+width: 45%;
+margin: 0 auto;
+border: 1px solid;
+padding: 1%;
+background-color: #1b1b1b;
+`
+
+const NamesStyles = styled.div`
+display: inline;
+border: 1 px solid black;
+color: white;
+font-size: 1.5rem;
+margin:2%;
+`
+
+const NamesStyles2 = styled.div`
+display: inline;
+justify-content: flex-end;
+border: 1 px solid black;
+color: white;
+font-size: 1.2rem;
+margin:1%;
+border: 1px solid white;
+padding: 1%;
+background-color: grey;
+border-radius: 40px;
+color: purple;
+`
+
+const Div = styled.div`
+display:flex;
+justify-content: space-between;
+border: 2px solid green;
+background-color: black;
+height: 5vh;
+margin-top: 1.5%;
+
+`
+
+function Character(props){
+    const {people} = props
+    return(
+       <StyledDiv>
+        {
+            people.map(char=>{
+                return(<Div>
+                    <NamesStyles>{char.name}</NamesStyles><NamesStyles2> {char.birth_year}</NamesStyles2>
+                    </Div>
+                )
+            })
+        }
+       </StyledDiv>
+    )
+
+
+
+
+
+
+}
+
+export default Character
+
+
+
+
